0637fdf28abd22ebb8945cbcfc23c2748d84b372
yoot <turkishpatriotman2@gmail.com>
2026-04-08 21:35:33 +0300

    Update constants.js

--- global/constants.js
+++ global/constants.js
@@ -1,16 +1,16 @@
 const PROTOCOLS_AND_PORTS = {
-    "ftp:": 20,
-    "ftps:": 21,
-    "sftp:": 22,
-    "ssh:": 22,
-    "tftp:": 69, // nice
-    "http:": 80,
-    "ws:": 80,
-    "https:": 443,
-    "wss:": 433
-}
+  "ftp:": 20,
+  "ftps:": 21,
+  "sftp:": 22,
+  "ssh:": 22,
+  "tftp:": 69,
+  "http:": 80,
+  "ws:": 80,
+  "https:": 443,
+  "wss:": 443
+};
 
 export const getPortForProtocol = (protocol) => {
-    const lowercase_protocol_string = `${protocol}`.toLowerCase();
-    return PROTOCOLS_AND_PORTS[lowercase_protocol_string];
-}
+  const lowercaseProtocolString = `${protocol}`.toLowerCase();
+  return PROTOCOLS_AND_PORTS[lowercaseProtocolString];
+};
